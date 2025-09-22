8d5e45384c9fe6ab6362225a2e5fb0d7d21f771c
camelia-macariu <camelia.macariu@boaryardx.com>
2025-09-22 19:00:02 +0300

    2

---
+++ Infrastructure/CoffeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class CoffeeDbContext : DbContext
+    {
+        public CoffeeDbContext(DbContextOptions<CoffeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Coffee> Coffee { get; set; }
+    }
+}
